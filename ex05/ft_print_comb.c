cc37dba5818b461aa867653503a93879edadb790
Amadeu Varandas <amadeuvarandas@gmail.com>
2024-01-14 16:55:34 +0000

    Solved for the units

--- ex05/ft_print_comb.c
+++ ex05/ft_print_comb.c
@@ -13,11 +13,12 @@ void ft_print_comb(void)
     int i;
     for(int i = 100; i <= 120; i++)
     {
-        int dez = (i % 100) / 10;;
-        int uni = i - dez * 10;
         int cen = i / 100;
+        int dez = (i % 100) / 10;
+        int uni = i % 10;
+        
 
-        printf("%i - %i\n", i, dez);  
+        printf("%i - %i - %i\n", i, dez, uni);  
     }
     
 }
--- testgit.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-int main()
-{
-    printf("isto 'e um teste para o git hub\n");
-}
