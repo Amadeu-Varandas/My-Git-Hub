77bb48edd73ce15fc815661f86eba7283401dab2
Amadeu Varandas <amadeuvarandas@gmail.com>
2024-01-14 16:50:19 +0000

    Solved the 100's part of the problem

--- ex05/ft_print_comb.c
+++ ex05/ft_print_comb.c
@@ -10,15 +10,14 @@ int main(void)
 
 void ft_print_comb(void)
 {
-    for(int i = 1; i <= 50; i++)
+    int i;
+    for(int i = 100; i <= 120; i++)
     {
-        int dez = i / 10;
+        int dez = (i % 100) / 10;;
         int uni = i - dez * 10;
-        /*write(1, &uni, 1);*/
-        if (dez < uni)
-        {
-            printf("%i\n", i);
-        }
-        
+        int cen = i / 100;
+
+        printf("%i - %i\n", i, dez);  
     }
+    
 }
