98a81b787a32dca7c8586ff2aa342c76a2612ff6
Amadeu Varandas <amadeuvarandas@gmail.com>
2024-01-21 22:15:41 +0000

    Added answer for ex02

---
+++ C_02/ex02/ft_str_is_alpha.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+int ft_str_is_alpha(char *str);
+int check_ft_str_is_alpha(char c);
+
+int main(void)
+{
+    char *word = "string";
+    if (ft_str_is_alpha(word) == 1)
+    {
+        printf("Apenas contem letras!\n");
+    }
+    else{
+        printf("tambem contem outras coisas\n");
+    }
+
+}
+
+int ft_str_is_alpha(char *str)
+{
+    unsigned int i = 0;
+
+    while (str[i] != '\0')
+    {
+        if(check_ft_str_is_alpha(str[i])==0)
+        {
+            return 0;
+        }
+        i++;
+    }
+    return 1;
+    
+}
+
+int check_ft_str_is_alpha(char c)
+{
+    if ((c >= 'a') && (c<='z'))
+    {
+        return 1;
+    }
+    else if ((c >= 'A') && (c<='Z'))
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
