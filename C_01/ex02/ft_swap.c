97af72afbb4ff47d3d9f98b2597f5991b4f69679
Amadeu Varandas <amadeuvarandas@gmail.com>
2024-01-21 16:51:50 +0000

    added ex02

--- C_01/ex02/ft_swap.c
+++ C_01/ex02/ft_swap.c
@@ -1,18 +1,11 @@
-#include <stdio.h>
-
 void ft_swap(int *a, int *b);
 
 int main(void)
 {
-    int a = 1;
-    int b = 2;
-    printf("Antes a: %i\n", a);
-    printf("Antes b: %i\n", b);
+    int a;
+    int b;
 
     ft_swap(&a, &b);
-
-    printf("Depois a: %i\n", a);
-    printf("Depois b: %i\n", b);
 }
 
 void ft_swap(int *a, int *b)
